9dfdc4a91ff89a3a402807ecab72c528d48e7ed0
blinkfox <chenjiayin1990@163.com>
2018-05-02 16:52:52 +0800

    新增在SpringBoot中的Zealot的默认配置功能

---
+++ src/main/java/com/blinkfox/zealotboot/ZealotAutoConfiguration.java
@@ -0,0 +1,46 @@
+package com.blinkfox.zealotboot;
+
+import com.blinkfox.zealot.config.ZealotConfigManager;
+import com.blinkfox.zealot.config.entity.NormalConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Zealot自动配置类.
+ *
+ * @author blinkfox on 2018-05-02.
+ */
+@Configuration
+@EnableConfigurationProperties(ZealotProperties.class)
+public class ZealotAutoConfiguration {
+
+    /** ZealotProperties属性配置类的实例. */
+    private ZealotProperties properties;
+
+    /**
+     * 构造方法.
+     */
+    @Autowired
+    public ZealotAutoConfiguration(ZealotProperties properties) {
+        this.properties = properties;
+        this.doConfig();
+    }
+
+    /**
+     * 根据properties中的配置项做zealot的配置.
+     */
+    private void doConfig() {
+        // 普通类型的配置
+        NormalConfig.getInstance()
+                .setDebug(this.properties.isDebug())
+                .setPrintBanner(this.properties.isPrintBanner())
+                .setPrintSqlInfo(this.properties.isPrintSql());
+
+        // 配置xml和handler扫描的路径.
+        ZealotConfigManager.getInstance()
+                .initLoadXmlLocations(this.properties.getXmlLocations())
+                .initLoadHandlerLocations(this.properties.getHandlerLocations());
+    }
+
+}
---
+++ src/main/java/com/blinkfox/zealotboot/ZealotProperties.java
@@ -0,0 +1,89 @@
+package com.blinkfox.zealotboot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 用于在 application.properties 文件中的配置属性类，定义前缀为'zealot'.
+ *
+ * @author blinkfox on 2018-05-02.
+ */
+@ConfigurationProperties("zealot")
+public class ZealotProperties {
+
+    /** 是否开启调试模式，如果开启的话，每次调用都会实时从最新的xml文件中获取sql，默认值为false. */
+    private boolean debug;
+
+    /** 是否打印zealot的启动banner，在SpringBoot项目中默认设为false. */
+    private boolean printBanner;
+
+    /** 是否打印zealot的sql日志，默认为true. */
+    private boolean printSql;
+
+    /** zealot的XML文件所在的位置，多个用逗号隔开,可以是目录也可以是具体的xml文件，默认是zealot资源目录及子目录下的位置. */
+    private String xmlLocations;
+
+    /** zealot的自定义handler处理器所在的位置，多个用逗号隔开,可以是目录也可以是具体的java或class文件路径. */
+    private String handlerLocations;
+
+    /**
+     * 无参构造方法，构造对象时赋予默认值.
+     */
+    public ZealotProperties() {
+        this.printBanner = false;
+        this.printSql = true;
+    }
+
+    /**
+     * 重写的toString()方法.
+     *
+     * @return 字符串
+     */
+    @Override
+    public String toString() {
+        return String.format("debug:%b, printBanner:%b, printSql:%b, xmlLocations:%s, handlerLocations:%s",
+                debug, printBanner, printSql, xmlLocations, handlerLocations);
+    }
+
+    /* 以下是 getter 和 setter 方法. */
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
+
+    public boolean isPrintBanner() {
+        return printBanner;
+    }
+
+    public void setPrintBanner(boolean printBanner) {
+        this.printBanner = printBanner;
+    }
+
+    public boolean isPrintSql() {
+        return printSql;
+    }
+
+    public void setPrintSql(boolean printSql) {
+        this.printSql = printSql;
+    }
+
+    public String getXmlLocations() {
+        return xmlLocations;
+    }
+
+    public void setXmlLocations(String xmlLocations) {
+        this.xmlLocations = xmlLocations;
+    }
+
+    public String getHandlerLocations() {
+        return handlerLocations;
+    }
+
+    public void setHandlerLocations(String handlerLocations) {
+        this.handlerLocations = handlerLocations;
+    }
+
+}
